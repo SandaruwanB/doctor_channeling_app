c1ec93e02707e3bfe378de333741bdd1ca25f755
SandaruwanB <sandarusbandara110@gmail.com>
2024-05-31 01:24:49 +0530

    [DEV] 404 page

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,9 @@
 const route = require('express').Router();
 const HomeController = require('../controllers/homeController');
 
-route.get('*', (req,res)=>{res.render('notFound')});
+
 route.get('/', HomeController.getHome);
 route.get('/about', (req,res)=>{res.render('about')});
+route.get('*', (req,res)=>{res.render('notFound')});
 
 module.exports = route;
