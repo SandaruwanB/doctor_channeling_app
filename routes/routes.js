8589a3d613a1b8cf119bd4ea002c067ccaa04899
SandaruwanB <sandarusbandara110@gmail.com>
2024-05-31 02:01:04 +0530

    [DEV] login route and view

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,11 @@
 const route = require('express').Router();
-const HomeController = require('../controllers/homeController');
-
 
 route.get('/', (req,res)=>{res.render('index')});
 route.get('/about', (req,res)=>{res.render('about')});
 route.get('/contact', (req,res)=>{res.render('contact')});
 route.get('/doctors', (req,res)=>{res.render('doctors')});
 route.get('/services', (req,res)=>{res.render('services')});
+route.get('/login', (req,res)=>{res.render('login')});
 
 
 route.get('*', (req,res)=>{res.render('notFound')});
