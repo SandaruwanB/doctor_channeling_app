824f1372067fcc2dce344ec19ca13a4a858b6d4d
SandaruwanB <sandarusbandara110@gmail.com>
2024-05-31 01:21:36 +0530

    [DEV] page not found and about page

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 const route = require('express').Router();
 const HomeController = require('../controllers/homeController');
 
+route.get('*', (req,res)=>{res.render('notFound')});
 route.get('/', HomeController.getHome);
+route.get('/about', (req,res)=>{res.render('about')});
 
 module.exports = route;
