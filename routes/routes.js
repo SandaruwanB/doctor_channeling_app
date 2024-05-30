21f914b89baa801afa048a695d45eb9f6efb5480
SandaruwanB <sandarusbandara110@gmail.com>
2024-05-31 01:29:32 +0530

    [DEV] contact page with route

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,9 @@ const HomeController = require('../controllers/homeController');
 
 route.get('/', HomeController.getHome);
 route.get('/about', (req,res)=>{res.render('about')});
+route.get('/contact', (req,res)=>{res.render('contact')});
+
+
 route.get('*', (req,res)=>{res.render('notFound')});
 
 module.exports = route;
