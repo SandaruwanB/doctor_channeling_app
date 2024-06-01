7d6957c31db730c5a1e7f0f1cd19c2863b6349da
SandaruwanB <sandarusbandara110@gmail.com>
2024-06-01 16:52:26 +0530

    [DEV] request to backend

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,30 @@
+const User = require('../models/userModel')
+const bcrypt = require('bcrypt');
+
 module.exports.index = (req,res)=>{
     res.render('login');
 }
 
 module.exports.performLogin = (req,res)=>{
     const {email, password} = req.body;
-    res.json({result : "success"});
+
+    User.findOne({where : {email : email}}).then(user=>{
+        if(user === null){
+            res.json({result : "not"});
+        }
+        else{
+            bcrypt.compare(password,user.password).then(result=>{
+                if(result){
+                    res.json({result : "authenticated"});
+                }
+                else{
+                    res.json({result : "pass"});
+                }
+            }).catch(err=>{
+                console.log(err);
+            })
+        }
+    }).catch(err=>{
+        console.log(err);
+    })
 } 
