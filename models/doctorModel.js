c12d90eb08fcd5c6c70ec69f5ed5088844b1585d
SandaruwanB <sandarusbandara110@gmail.com>
2024-06-03 00:23:59 +0530

    [DEV] doctor model

---
+++ models/doctorModel.js
@@ -0,0 +1,35 @@
+const Sequalizer = require('sequelize');
+const {sequalize} = require('../database/sequalize');
+
+const doctor = sequalize.define("doctor", {
+    firstName : {
+        type : Sequalizer.STRING,
+        allowNull : false,
+    },
+    lastName : {
+        type : Sequalizer.STRING,
+        allowNull : false
+    },
+    address : {
+        type : Sequalizer.STRING,
+        allowNull : true
+    },
+    email : {
+        type : Sequalizer.STRING,
+        allowNull : true
+    },
+    contact : {
+        type : Sequalizer.STRING,
+        allowNull : true
+    },
+    workingHoursPerDay : {
+        type : Sequalizer.INTEGER,
+        allowNull : false
+    },
+    averageTimePerPatient : {
+        type : Sequalizer.FLOAT,
+        allowNull : false
+    }
+});
+
+module.exports = doctor;
