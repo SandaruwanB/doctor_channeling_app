6057ec780349dfdb5bb84f1a5baca38383cdeded
SandaruwanB <sandarusbandara110@gmail.com>
2024-06-10 15:00:28 +0530

    [ADD] email configurations to env

--- config.js
+++ config.js
@@ -8,4 +8,9 @@ module.exports = {
     dbPass : process.env.DB_USER_PASSWORD,
     dbHost : process.env.DB_HOST,
     authSecret : process.env.AUTH_SECRET,
+    mailHost : process.env.EMAIL_HOST,
+    mailHostPort : process.env.EMAIL_HOST_PORT,
+    secureMail : process.env.SECURE_MAIL,
+    mailUser : process.env.EMAIL_USER,
+    mailToken : process.env.EMAIL_APP_TOKEN,
 }
