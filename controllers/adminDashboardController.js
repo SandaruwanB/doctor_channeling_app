5f4c52bdcc2567d20c8eb1f04d3707960478fa3e
SandaruwanB <sandarusbandara110@gmail.com>
2024-06-12 14:03:43 +0530

    [DEV] dashboard content

--- controllers/adminDashboardController.js
+++ controllers/adminDashboardController.js
@@ -1,3 +1,22 @@
-module.exports.getView = (req,res)=>{
-    res.render('admin/dashboard');
+const {channeling, patient, doctor, payment} = require('../models/channelingModel');
+
+module.exports.getView = async (req,res)=>{
+    const limit = 5;
+    let totalPayment = 0;
+    const channels = await channeling.findAll();
+    const doctors = await doctor.findAll();
+    const payments = await payment.findAll();
+    const patients = await patient.findAll();
+
+    payments.map((value)=>{
+        totalPayment += value.amount;
+    });
+
+    const filterdData = await channeling.findAll({
+        include : [{model : doctor}, {model : patient}],
+        order : [['createdAt', 'DESC']],
+        limit
+    });
+
+    res.render('admin/dashboard', {channels, doctors, payments, patients, totalPayment, filterdData});
 }
